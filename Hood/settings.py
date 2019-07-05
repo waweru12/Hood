a4556d41d158e76990d3891b7549d9575a24994d
Santa Claus <santa@northpole.com>
2019-07-05 16:19:46 +0300

    added requireed installations to settings.py

--- Hood/settings.py
+++ Hood/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'bootstrap4'
+    'Thehood'
+]    
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
