b651c7726e3c66ae6312880284dfcaf159c6774a
Santa Claus <santa@northpole.com>
2019-07-06 17:33:08 +0300

    installed bootsrap

--- Hood/settings.py
+++ Hood/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4',
-    'Thehood'
+    
+    'Thehood',
 ]    
 
 MIDDLEWARE = [
@@ -125,3 +125,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+)
