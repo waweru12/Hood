d8f38795232fe1c0d3cadfbf0424fcb89d8cc958
Santa Claus <santa@northpole.com>
2019-07-06 22:33:29 +0300

    created profile html

--- Thehood/models.py
+++ Thehood/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(blank=True,upload_to = 'images/',default = 'images/beauty1.jpg')
+   
+    bio = models.CharField(max_length = 255)
+    location = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return f'{self.user.username}'
--- Thehood/views.py
+++ Thehood/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
 def home (request):
     return render(request,'index.html')
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user=request.user
+    details=HoodDetails.objects.all()
+    posts=Post.objects.all()
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        postform = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        if postform.is_valid():
+            post = postform.save(commit=False)
+            post.user = current_user
+            post.neighbourhood = current_user.profile.neighbourhood
+            post.save()
+    else:
+        form = ProfileForm()
+        postform = PostForm()
+    return render(request, 'profile.html',{"form":form,"posts":posts,"postform":postform,"details":details})
